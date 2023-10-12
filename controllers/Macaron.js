a8c6aa408ddd5487528bf3841ac43eca005aed88
caroline <carolinedlf@gmail.com>
2023-10-12 21:48:27 +0200

    CRUD macarons

--- controllers/Macaron.js
+++ controllers/Macaron.js
@@ -1,19 +1,36 @@
 const Macaron = require('../models/Macaron')
 
 //ici les methodes async
-async function index(req,res){}
+async function index(req,res){
+    let macarons = await Macaron.find({})
+    res.send(macarons)
+}
 
-async function show(req,res){}
+async function show(req,res){
+    let {...params} = req.params
+    let macaron = await Macaron.findOne({...params})
+    res.send(macaron)
+}
 
 async function create(req,res){
     let {...data} = req.body
     await Macaron.create({...data})
-    res.sendStatus(201)
+    res.send("Apparition nouveau macaron")
 }
 
-async function edit(req,res){}
+async function edit(req,res){
+    let {...params} = req.params
+    let {...updatedData} = req.body
+    await Macaron.findOneAndUpdate({...params},{...updatedData})
+    res.send("il était pas beau mon macaron?")
 
-async function remove(req,res){}
+}
+
+async function remove(req,res){
+    let {...params} = req.params
+    await Macaron.findOneAndDelete({...params})
+    res.send("pas gentil")
+}
 
 
 module.exports = {index, show, create, edit, remove}
--- routes/macarons.js
+++ routes/macarons.js
@@ -5,7 +5,12 @@ const Macaron = require('../models/Macaron')
 const {index, show, create, edit, remove} = require('../controllers/Macaron') //methodes
 
 // les routes ici
-router.post('/add', create)
 
+router.get('/', index)
+router.get('/:name', show)
+
+router.post('/add', create)
+router.put('/edit/:name', edit)
+router.delete('/delete/:name', remove)
 
 module.exports = router
